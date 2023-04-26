5c53520f3e9fffb8a8af4cf02809ad326e43401f
Nayal Merchant <menayal1999@gmail.com>
2023-04-25 22:44:15 -0500

    updated env import

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import requests
 import config
 import json
+from decouple import config
 #Gets the data from the API and stores it in a file. Currently not for real time use...
+
+
+
 def findHomesIn60139():
 
     url = "https://us-real-estate.p.rapidapi.com/v2/for-sale-by-zipcode"
@@ -9,8 +13,8 @@ def findHomesIn60139():
     querystring = {"zipcode": "60139", "offset": "0", "limit": "10"}
 
     headers = {
-        "X-RapidAPI-Key": config.X_RapidAPI_Key,
-        "X-RapidAPI-Host": config.X_RapidAPI_Host
+        "X-RapidAPI-Key": config('X_RapidAPI_Key'),
+        "X-RapidAPI-Host": config('X_RapidAPI_Host')
     }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
